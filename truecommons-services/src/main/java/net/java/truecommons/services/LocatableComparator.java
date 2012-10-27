e8b13a2f4776ccf556871b2f536ae25096cdd188
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-27 19:43:11 +0200

    Spin-off of TrueCommons Services Annotations

---
+++ truecommons-services/src/main/java/net/java/truecommons/services/LocatableComparator.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.services;
+
+import java.util.Comparator;
+
+/**
+ * Compares {@link Locatable}s.
+ *
+ * @author Christian Schlichtherle
+ */
+class LocatableComparator implements Comparator<Locatable> {
+    @Override
+    public int compare(final Locatable o1, final Locatable o2) {
+        final int p1 = o1.getPriority();
+        final int p2 = o2.getPriority();
+        return p1 < p2 ? -1 : p1 == p2 ? 0 : 1;
+    }
+}
